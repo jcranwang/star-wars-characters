60d528709753c6b63d4672458d78d3925c849bbd
jcranwang <jason.cr.wang@gmail.com>
2018-08-10 22:11:18 +0930

    Adding title

--- star-wars-characters/src/containers/App.js
+++ star-wars-characters/src/containers/App.js
@@ -6,6 +6,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="bg-black mb3">
+          <div className="mw-100">
+            <img
+              className="h5"
+              src="http://imageshack.com/a/img922/3783/oyvsRd.png"
+              alt="Star Wars"
+            />
+          </div>
+          <h2 className="white code static dib">Characters Collection</h2>
+        </div>
         <Card name="Luke Skywalker" height="172" homeworld="Tatooine" />
       </div>
     );
