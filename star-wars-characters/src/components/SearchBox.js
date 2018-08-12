26625db8ed92fd0ca79bb53f79c2badbae55b128
jcranwang <jason.cr.wang@gmail.com>
2018-08-12 23:33:29 +0930

    Create SearchBox component, change title section

---
+++ star-wars-characters/src/components/SearchBox.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SearchBox = () => {
+  return (
+    <div className="pa2">
+      <input
+        type="search"
+        placeholder="Name"
+        className="pa2 ba b--black"
+      />
+    </div>
+  );
+};
+
+export default SearchBox;
